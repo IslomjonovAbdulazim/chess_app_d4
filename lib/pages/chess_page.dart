993fc83aaab39cc6c81164641ab7d4e04c667cad
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-21 17:17:26 +0500

    imp

---
+++ lib/pages/chess_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ChessPage extends StatefulWidget {
+  const ChessPage({super.key});
+
+  @override
+  State<ChessPage> createState() => _ChessPageState();
+}
+
+class _ChessPageState extends State<ChessPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SafeArea(
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
